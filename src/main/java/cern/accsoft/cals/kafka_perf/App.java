59013ab9fdc4c6e27db671489cfcc2503df33e9e
BlueEyedHush <blueeyedhush@gmail.com>
2016-07-08 07:03:31 +0200

    added kafka & logback dependency

--- src/main/java/cern/accsoft/cals/kafka_perf/App.java
+++ src/main/java/cern/accsoft/cals/kafka_perf/App.java
@@ -1,13 +1,22 @@
 package cern.accsoft.cals.kafka_perf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger("main");
+
+    public static void main( String[] args ) {
+        LOGGER.info("Application started");
+        new App().start();
+        LOGGER.info("Application terminated");
+    }
+
+    private void start() {
+
     }
 }
