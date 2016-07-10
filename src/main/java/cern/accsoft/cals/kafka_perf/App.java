ecd369cf8b10cd3d30f5dde2f737b8cb1d4c836d
BlueEyedHush <blueeyedhush@gmail.com>
2016-07-10 22:55:44 +0200

    added switch to enable raw printing of thoughput

--- src/main/java/cern/accsoft/cals/kafka_perf/App.java
+++ src/main/java/cern/accsoft/cals/kafka_perf/App.java
@@ -2,14 +2,16 @@ package cern.accsoft.cals.kafka_perf;
 
 import cern.accsoft.cals.kafka_perf.collectors.TimingCollector;
 import cern.accsoft.cals.kafka_perf.printers.PrettyThroughputPrinter;
+import cern.accsoft.cals.kafka_perf.printers.RawThroughtputPrinter;
 import com.martiansoftware.jsap.*;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.function.Consumer;
+
 /**
  * Hello world!
- *
  */
 public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger("main");
@@ -17,8 +19,9 @@ public class App {
     private static final String REPS_OPT = "reps";
     private static final String SERIES_OPT = "series";
     private static final String THREADS_OPT = "threads";
+    private static final String RAW_REPORTER_OPT = "raw_rep";
 
-    public static void main( String[] args ) throws Exception {
+    public static void main(String[] args) throws Exception {
         LOGGER.info("Application started");
         new App().start(getCliOptions(args));
         LOGGER.info("Application terminated");
@@ -26,18 +29,20 @@ public class App {
 
     private static JSAPResult getCliOptions(String[] args) throws Exception {
         SimpleJSAP jsap = new SimpleJSAP("Kafka performance tester", "",
-                new Parameter[] {
-                        new FlaggedOption( SERIES_OPT, JSAP.INTEGER_PARSER, "1", JSAP.NOT_REQUIRED, 's', JSAP.NO_LONGFLAG,
-                                "Number of test series. Duration of the whole series is timed." ),
-                        new FlaggedOption( REPS_OPT, JSAP.INTEGER_PARSER, "1000", JSAP.NOT_REQUIRED, 'r', JSAP.NO_LONGFLAG,
-                                "Number of repetitions in each series" ),
-                        new FlaggedOption( THREADS_OPT, JSAP.INTEGER_PARSER, "1", JSAP.NOT_REQUIRED, 't', JSAP.NO_LONGFLAG,
-                                "Number of threads sending messages" )
+                new Parameter[]{
+                        new FlaggedOption(SERIES_OPT, JSAP.INTEGER_PARSER, "1", JSAP.NOT_REQUIRED, 's', JSAP.NO_LONGFLAG,
+                                "Number of test series. Duration of the whole series is timed."),
+                        new FlaggedOption(REPS_OPT, JSAP.INTEGER_PARSER, "1000", JSAP.NOT_REQUIRED, 'r', JSAP.NO_LONGFLAG,
+                                "Number of repetitions in each series"),
+                        new FlaggedOption(THREADS_OPT, JSAP.INTEGER_PARSER, "1", JSAP.NOT_REQUIRED, 't', JSAP.NO_LONGFLAG,
+                                "Number of threads sending messages"),
+                        new Switch(RAW_REPORTER_OPT, 'R', JSAP.NO_LONGFLAG,
+                                "Should raw throughput printer be used instead of pretty")
                 }
         );
 
         JSAPResult config = jsap.parse(args);
-        if(jsap.messagePrinted()) System.exit(1);
+        if (jsap.messagePrinted()) System.exit(1);
 
         return config;
     }
@@ -46,11 +51,13 @@ public class App {
         final int series = config.getInt(SERIES_OPT);
         final int reps = config.getInt(REPS_OPT);
         final int threads = config.getInt(THREADS_OPT);
+        final Consumer<Double> printer = config.getBoolean(RAW_REPORTER_OPT) ?
+                new RawThroughtputPrinter() : new PrettyThroughputPrinter();
 
         TimingCollector c = new TimingCollector();
-        Reporter r = new Reporter(c, 3, reps, new PrettyThroughputPrinter());
+        Reporter r = new Reporter(c, 3, reps, printer);
 
-        for(int i = 0; i < threads; i++) {
+        for (int i = 0; i < threads; i++) {
             BenchmarkingProducer.createAndSpawnOnNewThread(() -> new ProducerRecord<String, String>("test_topic", "MSG"),
                     reps,
                     series,
