782f0a51a78ad1906fd7ab186550e06622bed13a
BlueEyedHush <blueeyedhush@gmail.com>
2016-07-11 16:25:27 +0200

    better way to supply the public key

--- src/main/bash/pull_results.sh
+++ src/main/bash/pull_results.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+# requires key.sh, which sets KEYFILE variable, pointing to the public key
+
 HOSTS="root@128.142.134.55 root@128.142.242.119 root@188.184.165.208"
+
+source ./keyfile.sh
 
 i=0
 
 for host in $HOSTS; do
-    scp "$*" ${host}:/tmp/results tmp
+    scp -i${KEYFILE} ${host}:/tmp/results tmp
     mv tmp/results tmp/$i
     ((i+=1))
 done
