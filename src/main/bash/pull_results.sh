ff5a65a6641be824eb53f862fcc26883b55e0c8d
BlueEyedHush <blueeyedhush@gmail.com>
2016-07-12 00:31:51 +0200

    changed script using for collecting results

--- src/main/bash/pull_results.sh
+++ src/main/bash/pull_results.sh
@@ -5,12 +5,11 @@
 HOSTS="root@128.142.134.55 root@128.142.242.119 root@188.184.165.208"
 
 source ./keyfile.sh
-
 i=0
 
 for host in $HOSTS; do
-    scp -i${KEYFILE} ${host}:/tmp/results tmp
-    mv tmp/results tmp/$i
+    mkdir -p tmp/${i}/
+    scp -i${KEYFILE} ${host}:/tmp/results/\* tmp/${i}/
     ((i+=1))
 done
 
