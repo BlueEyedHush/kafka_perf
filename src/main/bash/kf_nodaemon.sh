4d83f02dda72104773ffd59b52b9cb49d3d22855
BlueEyedHush <blueeyedhush@gmail.com>
2016-07-12 16:28:13 +0200

    wrong format for heap size, fixing

--- src/main/bash/kf_nodaemon.sh
+++ src/main/bash/kf_nodaemon.sh
@@ -5,6 +5,6 @@ source $DIR/env.sh
 
 mkdir -p $KAFKA_LOG_DIR
 
-export KAFKA_HEAP_OPTS="-Xmx1.5G -Xms1G"
+export KAFKA_HEAP_OPTS="-Xmx1536M -Xms1G"
 $KAFKA_DIR/bin/kafka-server-start.sh $KAFKA_DIR/config/server.properties 2>&1 | tee $KAFKA_LOG_DIR/kafka.log
 
