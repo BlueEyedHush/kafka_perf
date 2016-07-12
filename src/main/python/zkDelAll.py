f69aaf96db685d804f825ee916f63bbaf3e4968c
BlueEyedHush <blueeyedhush@gmail.com>
2016-07-12 10:35:17 +0200

    fixing error in zkDelAll - delete was used with recursive=False

--- src/main/python/zkDelAll.py
+++ src/main/python/zkDelAll.py
@@ -10,6 +10,6 @@ zk.start()
 for child in zk.get_children(root):
     child_path = root + child
     print('Deleting ' + child_path)
-    zk.delete(child_path)
+    zk.delete(path=child_path, recursive=True)
 
 zk.stop()
