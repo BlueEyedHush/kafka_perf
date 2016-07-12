219442e6879001a92e9e9b5e433d2233c5b4ef50
BlueEyedHush <blueeyedhush@gmail.com>
2016-07-12 16:26:31 +0200

    increasing heap size for kafka from 1G to 1.5G

--- src/main/bash/kf_nodaemon.sh
+++ src/main/bash/kf_nodaemon.sh
@@ -4,5 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/env.sh
 
 mkdir -p $KAFKA_LOG_DIR
+
+export KAFKA_HEAP_OPTS="-Xmx1.5G -Xms1G"
 $KAFKA_DIR/bin/kafka-server-start.sh $KAFKA_DIR/config/server.properties 2>&1 | tee $KAFKA_LOG_DIR/kafka.log
 
