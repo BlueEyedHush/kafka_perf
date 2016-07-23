ddf07c025098de1f2be453e0c5d3054c5e95e034
BlueEyedHush <blueeyedhush@gmail.com>
2016-07-23 10:15:15 +0200

    corrected some bugs in the script

--- bench_interactive.sh
+++ bench_interactive.sh
@@ -7,16 +7,28 @@ SUMMARY_FILE=$SUITE_DIR/summary.out
 
 mkdir -p $SUITE_DIR
 
+if [ $# -eq 2 ]; then
+    SEP=""
+else
+    SEP=","
+fi
+
 # arguments
 TASK_NAME=$1
 shift
 
-ARGS="suite_log_dir=$SUITE_DIR"
+if [[ $TASK_NAME == run_test_suite ]]; then
+    ARGS="suite_log_dir=$SUITE_DIR"
+else
+    ARGS=""
+fi
+
 for arg in $*; do
     COMMAS_ESCAPED=$(echo "$arg" | sed -e 's/,/\\,/g')
-    ARGS=$ARGS",$COMMAS_ESCAPED"
+    ARGS=$ARGS$SEP"$COMMAS_ESCAPED"
 done
 
 echo -e "\n\n\n------------------- "`date`"------------------- \n\n\n" >> $SUMMARY_FILE
 export PYTHONUNBUFFERED=true
-fab $TASK_NAME:"$ARGS" 2>&1 | tee -a $SUMMARY_FILE
+FAB_ARGS=$TASK_NAME:"$ARGS"
+fab $FAB_ARGS 2>&1 | tee -a $SUMMARY_FILE
